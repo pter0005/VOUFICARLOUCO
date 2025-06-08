97e8bf8bda4cc453ae783bb919ded729485d6e11
pedro henrique <dnlsniperonly2@gmail.com>
2025-06-08 06:40:56 +0000

    /** @type {import('next').NextConfig} */ const nextConfig = {   images:

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Mantendo a configuração existente
+  images: {
+    domains: ['i.imgur.com'],
+  },
 }
+
+module.exports = nextConfig;
